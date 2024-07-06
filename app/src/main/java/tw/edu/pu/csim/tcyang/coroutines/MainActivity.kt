b9b320a570ade14661d5551657093396c2b94409
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-07-06 13:38:29 +0800

    v1a_按下按鈕加1

--- app/src/main/java/tw/edu/pu/csim/tcyang/coroutines/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/coroutines/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.coroutines.ui.theme.CoroutinesTheme
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoroutinesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +34,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Greeting(modifier: Modifier) {
+    var counter by remember { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoroutinesTheme {
-        Greeting("Android")
+    Column {
+        Button(
+            modifier = modifier,
+            onClick = {
+                counter ++
+            }
+        ) {
+            Text(text = "加1")
+        }
+        Text(text = counter.toString())
     }
+
 }
